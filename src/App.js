b22a7df7d05fb7faf115ac3d31343da53ef5bf46
garyforce <gary.work.pro@gmail.com>
2023-02-03 07:41:29 -0500

    add SideBar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Routes,
-} from "react-router-dom";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { useState } from "react";
 import Home from "./pages/Home";
 import Location from "./pages/Location";
 import "./App.css";
 
 function App() {
+  const [isActiveSide, setIsActiveSide] = useState(false);
+  const hideSidebar = (e) => {
+    if (e.target.className == "sideBar") return;
+    e.preventDefault();
+    setIsActiveSide(false);
+  };
   return (
     <Router>
+      <input type="text" name="search" placeholder="Input City ZipCode" />
+      {isActiveSide && (
+        <div className="sideBar-background" onClick={hideSidebar}>
+          <div className="sideBar">
+            <button onClick={() => setIsActiveSide(false)}>&times;</button>
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+      )}
+      <button onClick={() => setIsActiveSide(true)}>&#9776;</button>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/favorites" element={<Location />} />
+        <Route path="/location" element={<Location />} />
       </Routes>
     </Router>
   );
--- src/utils.js/request.js
+++ src/utils.js/request.js
@@ -1,46 +1,62 @@
 import axios from "axios";
 
 const API_KEY = "OhyJ256EB3uyG7yd6YpMSUKbOA4Ma3At";
-const LOCATION_KEY = 07753;
+const LOCATION_KEY = "07753";
 
 const getCurrentCondition = async (location = LOCATION_KEY) => {
-  axios
-    .get(
+  let result = null;
+  try {
+    const response = await axios.get(
       `http://dataservice.accuweather.com/currentconditions/v1/${location}?apikey=${API_KEY}`
-    )
-    .then((res) => {
-      return res.data;
-    });
+    );
+
+    result = response.data;
+    return result[0];
+  } catch (error) {
+    return error;
+  }
 };
 
 const getFiveDayForecast = async (location = LOCATION_KEY) => {
-  axios
-    .get(
+  let result = null;
+  try {
+    const response = await axios.get(
       `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${location}?apikey=${API_KEY}`
-    )
-    .then((res) => {
-      return res.data;
-    });
+    );
+
+    result = response.data;
+    return result;
+  } catch (error) {
+    return error;
+  }
 };
 
 const getDailyForecast = async (location = LOCATION_KEY) => {
-  axios
-    .get(
+  let result = null;
+  try {
+    const response = await axios.get(
       `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${location}?apikey=${API_KEY}`
-    )
-    .then((res) => {
-      return res.data;
-    });
+    );
+
+    result = response.data;
+    return result[0];
+  } catch (error) {
+    return error;
+  }
 };
 
 const getHourlyForecast = async (location = LOCATION_KEY) => {
-  axios
-    .get(
+  let result = null;
+  try {
+    const response = await axios.get(
       `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${location}?apikey=${API_KEY}`
-    )
-    .then((res) => {
-      return res.data;
-    });
+    );
+
+    result = response.data;
+    return result[0];
+  } catch (error) {
+    return error;
+  }
 };
 
 export {
